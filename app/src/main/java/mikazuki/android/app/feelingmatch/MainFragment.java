f8475a2674431419fcb20af33ac3e8830eafb096
HAIJIMA Kazuki <k.haijima@gmail.com>
2015-11-17 23:51:51 +0900

    Initial setting

---
+++ app/src/main/java/mikazuki/android/app/feelingmatch/MainFragment.java
@@ -0,0 +1,30 @@
+package mikazuki.android.app.feelingmatch;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by haijimakazuki on 15/11/17.
+ */
+public class MainFragment extends Fragment {
+
+
+
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.content_main, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+
+}
