9ba336f2107e709c809a7415f8d9084d21b64897
haijima <k.haijima@gmail.com>
2016-03-17 19:59:14 +0900

    Introduce Stetho-realm that is forked and updated to display Date and LinkList

---
+++ app/src/main/java/mikazuki/android/app/feelingmatch/FeelingMatchApplication.java
@@ -0,0 +1,23 @@
+package mikazuki.android.app.feelingmatch;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+/**
+ * @author haijimakazuki
+ */
+public class FeelingMatchApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).withDescendingOrder().build())
+                        .build());
+    }
+}
